f0556ca31d3c207ae342ea4b43e4a8aa37673187
alanshalem <alanshalem@gmail.com>
2022-06-20 16:11:08 -0300

    first commit

---
+++ reddit_sentiment_analysis_trading_bot/reddittradingbot1.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="CLIENT_ID",
+    client_secret="CLIENT_SECRET",
+    password="PASSWORD",
+    user_agent="USERAGENT",
+    username="USERNAME",
+)
